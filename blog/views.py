5068d997c1d7d76950750be7818685e4afee4c8f
FontanaAlessandro <fontana.alessandro@studenti.salesianisesto.it>
2022-06-14 15:31:20 +0200

    aggiunto traktor

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,12 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
-    posts = Post.objects.filter(data_pubblicazione__lte=timezone.now()).order_by('data_pubblicazione')
+    posts = Post.objects.filter(data_pubblicazione__lte=timezone.now()).order_by('-data_pubblicazione')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if pk == 1:
+        return render(request, 'blog/post_detail.html', {'post': post})
+    else:
+        return render(request, 'blog/post2_detail.html', {'post': post})
